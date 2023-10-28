9e4ca6e91ed596c23548be67b92a8e776196dea8
Alvin Yap <leaf.alvin@gmail.com>
2023-10-28 22:52:18 +0800

    feat: update buttons

--- src/components/digitButton.js
+++ src/components/digitButton.js
@@ -4,6 +4,8 @@ export default function DigitButton({ dispatch, digit }) {
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
-    ></button>
+    >
+      {digit}
+    </button>
   );
 }
